0d90898ea3ae9968a498cb2a73992fdbd27d8ecc
aesaraesaraesar <aesar12345a@gmail.com>
2025-03-08 04:57:25 -0800

    Update casery.py

--- casery.py
+++ casery.py
@@ -11,6 +11,6 @@ group = "https://t.me/helashelb"
 ##لوجو السورس
 photosource = "https://telegra.ph/file/79d346793ec3e73d5e9f0.jpg"
 ##توكن الصانع
-bot_token="6452378945:AAEhJvci0E3hzohh6rp_Oq4S3qLfTdnzxss"
+bot_token="6318547194:AAF4QIlS2E7c6UyBk6l9w8c1iCOVokbB0Mo"
 ##جلسه الاشتراك
 bot_token2="BAGhJBcAN2r7JAs_rO18MgKPyqXQ2WQ6ArSjPXe4Ljk3w-c7Wktgd7NSQowbV7aY1c4MwD-ym6APzubAbwzAsusmX8dqidLg1GQLpvLQvJhhdiUnRZnIHSTSZd5K9JRFrWVgxz0nAL3nPUhVcLr-4-FUTlBCSsHh9VEXauD2QbTFF8nIjS5Gd9G-mkvUlBwbK2fn5qel2z3bbNUIQFod9xntmO4B9rFYq5Z0x_w-97GWHVRI2n81kY4q1FJCimyQuwxpeacGBtcoKVQaG21Nb4J7p_w225mPEXT-een7VtHnyjCkFyBl0ybDmqJs_PPJqszRrlc2ca5hIb8eL5sXpKTHLukCYwAAAAG89nyFAA"
